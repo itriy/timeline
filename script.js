1686b64ed8857f54dd784ab15916cb3285251b97
sanya <sanya@MacBook-Pro.local>
2016-08-10 12:23:25 +0300

    refactoring

--- script.js
+++ script.js
@@ -1,119 +1,120 @@
-class TimeLine {
-  
-  constructor(container, options) {
+(function(){
+  class TimeLine {
+    
+    constructor(container, options) {
 
-    this.container = document.getElementById(container);
-    this.minDocumentWidth = options.minDocumentWidth || true;
-    this.wrapper = this.container.querySelector('.wrapper');
-    this.tooltipHeight = parseInt((getComputedStyle(this.container.querySelector('.tooltip')).height), 10);
-    this.shiftTooltip = 0;
-    this.setTimeLine(this.wrapper);
+      this.container = document.querySelector(container);
+      this.minDocumentWidth = options.minDocumentWidth || true;
+      this.wrapper = this.container.querySelector('.tl-wrapper');
 
-    window.addEventListener("resize", this.setTimeLine.bind(this, this.wrapper));
+      this.tooltipHeight = parseInt((getComputedStyle(this.container.querySelector('.tl-tooltip')).height), 10);
+      this.shiftTooltip = 0;
+      this.setTimeLine(this.wrapper);
 
-  }
+      window.addEventListener("resize", this.setTimeLine.bind(this, this.wrapper));
 
-  setTimeLine(wrapper) {
-    let elems = wrapper.querySelectorAll('.element-wrapper');
+    }
 
-    let contentLeft = 0,
+    setTimeLine(wrapper) {
+
+      let elems = wrapper.querySelectorAll('.tl-element-wrapper');
+
+      let contentLeft = 0,
         contentRight = 0;
 
-    let body = this.elementSize(document.body);
-    
-    for (let i = 0; i < elems.length; i++) {
+      let body = this.elementSize(document.body);
 
-      let elem = elems[i].querySelector('.element');
-      let tooltip = elems[i].querySelector('.tooltip');
-      let size = this.elementSize(elem);
+      for (let i = 0; i < elems.length; i++) {
 
-      elems[i].classList.remove('block-right');
-      elems[i].classList.remove('block-left');
-      this.wrapper.classList.add('tl-mobile');
+        let elem = elems[i].querySelector('.tl-element');
+        let tooltip = elems[i].querySelector('.tl-tooltip');
+        let size = this.elementSize(elem);
 
-      tooltip.style.top = '';
-      elem.style.top = '';
-      this.wrapper.style.height = '';
+        elems[i].classList.remove('tl-block-right');
+        elems[i].classList.remove('tl-block-left');
+        this.wrapper.classList.add('tl-mobile');
+        tooltip.style.top = '';
+        elem.style.top = '';
+        this.wrapper.style.height = '';
 
-      if( body.width >= this.minDocumentWidth){
-        if (contentLeft <= contentRight) {
+        if( body.width >= this.minDocumentWidth){
+          if (contentLeft <= contentRight) {
 
-          elem.style.top = contentLeft + 'px';
-          elems[i].classList.add('block-left');
-          this.tooltipPosition(contentLeft);
-          contentLeft += size.height;
+            elem.style.top = contentLeft + 'px';
+            elems[i].classList.add('tl-block-left');
+            this.tooltipPosition(contentLeft);
+            contentLeft += size.height;
 
-        } else {
+          } else {
 
-          elem.style.top = contentRight + 'px';
-          elems[i].classList.add('block-right');
-          this.tooltipPosition(contentRight);
-          contentRight += size.height;
+            elem.style.top = contentRight + 'px';
+            elems[i].classList.add('tl-block-right');
+            this.tooltipPosition(contentRight);
+            contentRight += size.height;
 
+          }
+          this.wrapper.classList.remove('tl-mobile');
+          tooltip.style.top = this.shiftTooltip + 'px';
         }
-        
-        this.wrapper.classList.remove('tl-mobile');
-        tooltip.style.top = this.shiftTooltip + 'px';
+
+
+      }
+      if(body.width >= this.minDocumentWidth){
+        let wrapperHeight = contentLeft > contentRight ? contentLeft : contentRight;
+        this.wrapper.style.height = wrapperHeight + 'px';
       }
 
-
-    }
-    if(body.width >= this.minDocumentWidth){
-      let wrapperHeight = contentLeft > contentRight ? contentLeft : contentRight;
-      this.wrapper.style.height = wrapperHeight + 'px';
     }
 
-  }
+    tooltipPosition(startCoords) {
 
-  tooltipPosition(startCoords) {
+      let oldShiftTooltip = this.shiftTooltip;
+      this.shiftTooltip = startCoords + this.tooltipHeight;
 
-    let oldShiftTooltip = this.shiftTooltip;
-    this.shiftTooltip = startCoords + this.tooltipHeight;
+      if (oldShiftTooltip > 0 && (this.shiftTooltip > oldShiftTooltip - this.tooltipHeight && this.shiftTooltip <= oldShiftTooltip + this.tooltipHeight)) {
 
-    if (oldShiftTooltip > 0 && (this.shiftTooltip > oldShiftTooltip - this.tooltipHeight && this.shiftTooltip <= oldShiftTooltip + this.tooltipHeight)) {
+        this.shiftTooltip = oldShiftTooltip + 2 * this.tooltipHeight;
 
-      this.shiftTooltip = oldShiftTooltip + 2 * this.tooltipHeight;
+      }
 
     }
 
-  }
+    getScrollWidth(){
+      let div = document.createElement('div');
 
-  getScrollWidth(){
-    let div = document.createElement('div');
-
-    div.style.overflowY = 'scroll';
-    div.style.width = '50px';
-    div.style.height = '50px';
-    div.style.visibility = 'hidden';
-    div.style.position = 'absolute';
-    div.style.top = '0px';
-    div.style.left = '0px';
+      div.style.overflowY = 'scroll';
+      div.style.width = '50px';
+      div.style.height = '50px';
+      div.style.visibility = 'hidden';
+      div.style.position = 'absolute';
+      div.style.top = '0px';
+      div.style.left = '0px';
 
 
 
-    document.body.appendChild(div);
-    let scrollWidth = div.offsetWidth - div.clientWidth;
-    let scrollHeight = div.offsetHeight - div.clientHeight;
-    document.body.removeChild(div);
+      document.body.appendChild(div);
+      let scrollWidth = div.offsetWidth - div.clientWidth;
+      let scrollHeight = div.offsetHeight - div.clientHeight;
+      document.body.removeChild(div);
 
-    return {width: scrollWidth, height: scrollHeight};
-  }
+      return {width: scrollWidth, height: scrollHeight};
+    }
 
-  elementSize(element) {
+    elementSize(element) {
 
-    let elem = getComputedStyle(element);
+      let elem = getComputedStyle(element);
 
-    this.scrollWidth = this.getScrollWidth();
+      this.scrollWidth = this.getScrollWidth();
 
-    return {
-      width: parseInt(elem.width, 10) + parseInt(elem.marginLeft, 10) + parseInt(elem.marginRight, 10) + this.scrollWidth.width,
-      height: parseInt(elem.height, 10) + parseInt(elem.marginBottom, 10) + parseInt(elem.marginTop, 10) + +this.scrollWidth.height
+      return {
+        width: parseInt(elem.width, 10) + parseInt(elem.marginLeft, 10) + parseInt(elem.marginRight, 10) + this.scrollWidth.width,
+        height: parseInt(elem.height, 10) + parseInt(elem.marginBottom, 10) + parseInt(elem.marginTop, 10) + +this.scrollWidth.height
+      }
+
     }
 
   }
+  window.TimeLine = TimeLine;
+})();
 
-}
-window.onload = function () { new TimeLine('timeline', {minDocumentWidth: 500}) }
-
-
-//new TimeLine(document.getElementById('timeline'));
+window.onload = function () { new TimeLine('#timeline', {minDocumentWidth: 500}) }
